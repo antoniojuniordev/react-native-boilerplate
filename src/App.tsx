2135ec641f5ebbc32cc3340677599a6fbc572cda
Junior <junior4g4@gmail.com>
2023-03-29 16:21:29 -0300

    feat: eslint ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
-  useColorScheme
-} from 'react-native'
-
+  useColorScheme,
+} from 'react-native';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import Routes from './routes/routes'
+import Routes from './routes/routes';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark'
+  const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1
-  }
+    flex: 1,
+  };
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -26,7 +25,7 @@ function App() {
 
       <Routes />
     </SafeAreaView>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 function Login() {
-  const { login } = useContext(AuthContext);
-
-  async function sendLogin() {
-    login();
-    setData(respose.data);
-  }
-
   return (
     <View style={{ flex: 1 }}>
       <Text style={{ fontSize: 22 }}>tela 1</Text>

227a6d8e967eeb95ea3f544b66bc14d954e4e474
Junior <junior4g4@gmail.com>
2023-04-02 09:05:11 -0300

    feat: add styled components

---
+++ src/screens/tela2/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+import * as Styled from './styles';
+
+function Tela2() {
+  return (
+    <Styled.First>
+      <Styled.Text>
+        tela 2
+      </Styled.Text>
+      <Text style={{ fontFamily: 'SourceSansPro-Black', fontSize: 32 }}> tessswsss</Text>
+    </Styled.First>
+  );
+}
+
+export default Tela2;
---
+++ src/screens/tela2/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Text = styled.Text`
+  margin-top: 20px;
+  font-size: 32px;
+  font-family: 'SourceSansPro-Black';
+`;
+
+export const First = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
